d22333eeda4f61a13476d7de4397748da2519750
CrazyGoat646 <mr.capu64@gmail.com>
2025-06-14 23:09:26 +0200

    Actualización: agregado logo, encuesta funcional, stream con imagen

--- src/routes/EventDetail.jsx
+++ src/routes/EventDetail.jsx
@@ -1,5 +1,5 @@
 import { useParams, Link } from "react-router-dom";
-import Header from "/components/Header";
+import Header from ".../components/Header";
 
 export default function EventDetail() {
   const { id } = useParams();
--- src/routes/Survey.jsx
+++ src/routes/Survey.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Header from "/components/Header";
+import Header from ".../components/Header";
 
 export default function Survey() {
   const [formData, setFormData] = useState({
