e6376b2839086f3d3004bd161d06b8c99985f567
CrazyGoat646 <mr.capu64@gmail.com>
2025-06-16 00:33:51 +0200

    Corregir

--- src/routes/Stream.jsx
+++ src/routes/Stream.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header";
-import equipoImage from "../assets/equipo.png";
+import equipoImage from "../assets/equipo.jpg";
 import tecladoImage from "/images/teclado.png";
 
 export default function Stream() {
